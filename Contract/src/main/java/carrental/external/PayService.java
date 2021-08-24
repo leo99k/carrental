fa5afd7b16bf55a2226f6dcf6044f6fb34f99597
leo99k <leo99k@nate.com>
2021-08-24 22:52:29 +0900

    11
    11

--- Contract/src/main/java/carrental/external/PayService.java
+++ Contract/src/main/java/carrental/external/PayService.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 // feignclient 기술 적용
 // url: http://localhost:8082 - application.yaml에 정의함
 
-@FeignClient(name = "Pay", url="http://localhost:8082") // 
+@FeignClient(name = "Pay", url="http://localhost:8082")  // 
 public interface PayService {
     @RequestMapping(method = RequestMethod.POST, path = "/pays", consumes = "application/json")
     public void pay(@RequestBody Pay pay);
